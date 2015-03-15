788dc6c2b137e62836cca9023d1c80047d13481c
Petr Chromec <petr.chromec@lmc.eu>
2015-03-15 19:22:55 +0100

    inserting form added

--- api/txtToDb.php
+++ api/txtToDb.php
@@ -1,18 +1,49 @@
 <?php
+error_reporting(E_ALL ^ E_NOTICE);
 
-$pdo = new PDO('mysql:host=localhost;dbname=fairytale', 'root', '');
+if ($_POST['save']) {
+    $pdo = new PDO('mysql:host=localhost;dbname=fairytale', 'root', '');
 
-$bookId = 1;
-$text = "";
-$chapterNumber = 1;
-$title = '';
+    $bookId = 1;
+    $text = $_POST['text'];
+    $chapterNumber = (int)$_POST['chapter'];
+    $title = $_POST['title'];
 
-$paragraphs = explode("\n", $text);
-$inserts = [];
+    $chapter = $pdo->query("INSERT INTO chapter (number, title, book_id, image) VALUES ($chapterNumber, '$title', $bookId, '')");
 
-foreach($paragraphs as $paragraph) {
-    $inserts[] = "('$chapterNumber', '$paragraph', '1')";
+    $paragraphs = explode("\n", $text);
+    $inserts = [];
+
+    foreach($paragraphs as $paragraph) {
+        $inserts[] = "('$chapterNumber', '$paragraph', '1')";
+    }
+
+    $pdo->query("INSERT INTO paragraph (chapter_id, content, is_new) VALUES " . implode(",", $inserts));
+
+    $status = 'ok';
 }
-
-$pdo->query("INSERT INTO chapter (number, title, book_id) VALUES ($chapterNumber, '$title', $bookId)");
-$pdo->query("INSERT INTO paragraph (chapter_id, content, is_new) VALUES " . implode(",", $inserts));
+?>
+<html>
+<head>
+    <title>Fairytale - insert chapters</title>
+</head>
+<body>
+<div>
+    <?=$status?>
+</div>
+<form action="" method="POST">
+    <div>
+        <input type="text" name="chapter" value="" placeholder="chapter" />
+        <input type="text" name="title" value="" placeholder="title" style="width: 200px;" />
+    </div>
+    
+    <div>
+        <textarea name="text" placeholder="text" style="width: 690px; height: 600px;"></textarea>
+    </div>
+    
+    <div>
+        <input type="submit" name="save" value="OK" style="width: 100px; height: 50px;" />
+    </div>
+</form>    
+</body>
+</html>
