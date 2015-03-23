61f19fb20699a1934261a0b9868bfebea82abeeb
Petr Chromec <petr.chromec@lmc.eu>
2015-03-23 19:48:21 +0100

    htaccess and config class added

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+
+class config
+{
+    /** @var PDO */
+    private $pdo;
+
+    /**
+     * @return string
+     */
+    public function getHomeUrl()
+    {
+        return 'http://localhost/Fairytale/';
+    }
+
+    /**
+     * @return PDO
+     */
+    public function getPdoConnection()
+    {
+        if (!isset($this->pdo)) {
+            $this->pdo = new PDO('mysql:host=localhost;dbname=fairytale;charset=utf8', 'root', '');
+        }
+
+        return $this->pdo;
+    }
+}
