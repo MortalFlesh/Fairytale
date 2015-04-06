1da1fba381d232c93579f5ba6a8b349600de8926
Petr Chromec <petr.chromec@lmc.eu>
2015-04-06 17:48:39 +0200

    paragraphs publisher separated to service

--- core/src/ChaptersUpdateAction.php
+++ core/src/ChaptersUpdateAction.php
@@ -2,6 +2,7 @@
 namespace MF\Fairytale;
 
 use MF\Fairytale\Exception\NothingToPublishException;
+use MF\Fairytale\Service\ParagraphPublisher;
 use MF\Fairytale\Service\ServiceStatus;
 use PDO;
 use DateTime;
@@ -12,10 +13,6 @@ class ChaptersUpdateAction
     const ALREADY_RUN = 'already_run';
     const TRY_TO_RUN = 'trying';
     const MAX_ATTEMPS = 5;
-
-    const MIN_LIMIT = 2;
-    const LIMIT_ROLLS = 3;
-    const PARAGRAPH_MIN_LENGTH = 150;
 
     /** @var string */
     private $status;
@@ -32,6 +29,9 @@ class ChaptersUpdateAction
     /** @var ServiceStatus */
     private $serviceStatus;
 
+    /** @var ParagraphPublisher */
+    private $publisher;
+
     /**
      * @param PDO $pdo
      * @param Dice $dice
@@ -44,6 +44,8 @@ class ChaptersUpdateAction
         $this->dice = $dice;
         $this->whatsAppService = $whatsAppService;
         $this->serviceStatus = $serviceStatus;
+
+        $this->publisher = new ParagraphPublisher($this->pdo, $this->dice);
     }
 
     /**
@@ -58,7 +60,9 @@ class ChaptersUpdateAction
         $this->setAllPublicAsOld();
 
         try {
-            $published = $this->publishParagraphs();
+            $this->serviceStatus->setStatus(__CLASS__, self::ALREADY_RUN);
+
+            $published = $this->publisher->publishParagraphs();
 
             if ($published > 0) {
                 $this->notify($published);
@@ -108,130 +112,9 @@ class ChaptersUpdateAction
         return false;
     }
 
-    /**
-     * @return int
-     */
-    private function publishParagraphs()
-    {
-        $chapterId = $this->getFirstChapterIdToPublic();
-        if ($chapterId <= 0) {
-            throw new NothingToPublishException();
-        }
-
-        $limit = $this->calcLimit();
-
-        $paragraphs = $this->loadParagraphsToPublish($chapterId, $limit);
-        $newLimit = $this->checkParagraphsAndGetNewLimit($paragraphs, $limit);
-
-        if ($newLimit !== $limit) {
-            $paragraphs = $this->loadParagraphsToPublish($chapterId, $newLimit);
-        }
-
-        $this->setParagraphsAsNewAndPublic($paragraphs);
-        $this->serviceStatus->setStatus(__CLASS__, self::ALREADY_RUN);
-
-        return count($paragraphs);
-    }
-
-    /**
-     * @return int
-     */
-    private function getFirstChapterIdToPublic()
-    {
-        $res = $this->pdo->query("
-            SELECT C.number
-            FROM chapter C
-            JOIN paragraph P
-                ON C.number = P.chapter_id
-                AND P.public = 0
-            ORDER BY C.number ASC
-            LIMIT 1
-            ");
-
-        return (int) $res->fetchColumn();
-    }
-
     private function setAllPublicAsOld()
     {
         $this->pdo->query("UPDATE paragraph SET is_new = 0");
-    }
-
-    /**
-     * @return int
-     */
-    private function calcLimit()
-    {
-        $limit = self::MIN_LIMIT;
-
-        for ($i = 0; $i < self::LIMIT_ROLLS; $i++) {
-            if ($this->dice->roll(6) === 6) {
-                $limit++;
-            }
-        }
-
-        return $limit;
-    }
-
-    /**
-     * @param $chapterId
-     * @param $limit
-     *
-     * @return array
-     */
-    private function loadParagraphsToPublish($chapterId, $limit)
-    {
-        $res = $this->pdo->query("
-            SELECT *
-            FROM paragraph
-            WHERE chapter_id >= $chapterId AND public = 0
-            LIMIT $limit
-        ");
-
-        return $res->fetchAll(PDO::FETCH_ASSOC);
-    }
-
-    /**
-     * @param array $paragraphs
-     * @param $limit
-     *
-     * @return mixed
-     */
-    private function checkParagraphsAndGetNewLimit(array $paragraphs, $limit)
-    {
-        $newLimit = $limit;
-
-        foreach ($paragraphs as $paragraph) {
-            if (mb_strlen($paragraph['content']) < self::PARAGRAPH_MIN_LENGTH) {
-                $newLimit++;
-            }
-        }
-
-        return $newLimit;
-    }
-
-    /**
-     * @param array $paragraphs
-     */
-    private function setParagraphsAsNewAndPublic(array $paragraphs)
-    {
-        $ids = [];
-        foreach ($paragraphs as $paragraph) {
-            $ids[] = (int) $paragraph['id'];
-        }
-
-        $now = new DateTime();
-        $nowFormated = $now->format(DB_DATE_FORMAT);
-
-        if (!empty($ids)) {
-            $this->pdo->query("
-                UPDATE paragraph
-                SET
-                    is_new = 1,
-                    public = 1,
-                    public_from = '$nowFormated'
-                WHERE id IN (" . implode(',', $ids) . ")
-            ");
-        }
     }
 
     /**
---
+++ core/src/Service/ParagraphPublisher.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace MF\Fairytale\Service;
+
+use DateTime;
+use MF\Fairytale\Dice;
+use MF\Fairytale\Exception\NothingToPublishException;
+use PDO;
+
+class ParagraphPublisher
+{
+    const MIN_LIMIT = 2;
+    const LIMIT_ROLLS = 3;
+    const PARAGRAPH_MIN_LENGTH = 150;
+
+    /** @var PDO */
+    private $pdo;
+
+    /** @var Dice */
+    private $dice;
+
+    /**
+     * @param PDO $pdo
+     * @param Dice $dice
+     */
+    public function __construct(PDO $pdo, Dice $dice)
+    {
+        $this->pdo = $pdo;
+        $this->dice = $dice;
+    }
+
+    /**
+     * @return int
+     */
+    public function publishParagraphs()
+    {
+        $chapterId = $this->getFirstChapterIdToPublic();
+        if ($chapterId <= 0) {
+            throw new NothingToPublishException();
+        }
+
+        $limit = $this->calcLimit();
+
+        $paragraphs = $this->loadParagraphsToPublish($chapterId, $limit);
+        $newLimit = $this->checkParagraphsAndGetNewLimit($paragraphs, $limit);
+
+        if ($newLimit !== $limit) {
+            $paragraphs = $this->loadParagraphsToPublish($chapterId, $newLimit);
+        }
+
+        $this->setParagraphsAsNewAndPublic($paragraphs);
+
+        return count($paragraphs);
+    }
+
+    /**
+     * @return int
+     */
+    private function getFirstChapterIdToPublic()
+    {
+        $res = $this->pdo->query("
+            SELECT C.number
+            FROM chapter C
+            JOIN paragraph P
+                ON C.number = P.chapter_id
+                AND P.public = 0
+            ORDER BY C.number ASC
+            LIMIT 1
+            ");
+
+        return (int) $res->fetchColumn();
+    }
+
+    /**
+     * @return int
+     */
+    private function calcLimit()
+    {
+        $limit = self::MIN_LIMIT;
+
+        for ($i = 0; $i < self::LIMIT_ROLLS; $i++) {
+            if ($this->dice->roll(6) === 6) {
+                $limit++;
+            }
+        }
+
+        return $limit;
+    }
+
+    /**
+     * @param $chapterId
+     * @param $limit
+     * @return array
+     */
+    private function loadParagraphsToPublish($chapterId, $limit)
+    {
+        $res = $this->pdo->query("
+            SELECT *
+            FROM paragraph
+            WHERE chapter_id >= $chapterId AND public = 0
+            LIMIT $limit
+        ");
+
+        return $res->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    /**
+     * @param array $paragraphs
+     * @param $limit
+     * @return mixed
+     */
+    private function checkParagraphsAndGetNewLimit(array $paragraphs, $limit)
+    {
+        $newLimit = $limit;
+
+        foreach ($paragraphs as $paragraph) {
+            if (mb_strlen($paragraph['content']) < self::PARAGRAPH_MIN_LENGTH) {
+                $newLimit++;
+            }
+        }
+
+        return $newLimit;
+    }
+
+    /**
+     * @param array $paragraphs
+     */
+    private function setParagraphsAsNewAndPublic(array $paragraphs)
+    {
+        $ids = [];
+        foreach ($paragraphs as $paragraph) {
+            $ids[] = (int) $paragraph['id'];
+        }
+
+        $now = new DateTime();
+        $nowFormated = $now->format(DB_DATE_FORMAT);
+
+        if (!empty($ids)) {
+            $this->pdo->query("
+                UPDATE paragraph
+                SET
+                    `is_new` = 1,
+                    `public` = 1,
+                    `public_from` = '$nowFormated'
+                WHERE `id` IN (" . implode(',', $ids) . ")
+            ");
+        }
+    }
+}
